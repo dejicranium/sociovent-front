8c7d654d3c7c996da8285298396a436a716b46a9
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-05-10 00:03:51 +0100

    a

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -42,7 +42,7 @@
 							<input type="text" class="form__control" v-model="signupData.twitter_handle" placeholder="">
 						</div>
 						<div class="form__div">
-							<button id="signupButton" type="button" class="form__control form__control__submit" @click="signup" >Sign up</button>
+							<button id="a" type="button" class="form__control form__control__submit" @click="signup">Sign up</button>
 						</div>
 
 					</form>
@@ -221,12 +221,12 @@ export default {
 		signup(e) {
 			this.authErrorMessage = '';
 			let queryString = '';
-			let l = document.getElementById('signupButton');
-			const original_text = l.innerText;
-			toggleButtonActiveness(l, original_text)
+			let button = document.getElementById('a');
+			const original_text = button.innerText;
+			toggleButtonActiveness(button, original_text)
 
 			if (!this.validateSignupFields()) {
-				toggleButtonActiveness(l, original_text)
+				toggleButtonActiveness(button, original_text)
 
 				return;
 			}
@@ -251,7 +251,7 @@ export default {
 					this.loginData.identifier = this.signupData.username;
 					this.loginData.password = this.signupData.password;
 					this.signin();
-					toggleButtonActiveness(l, original_text)
+					toggleButtonActiveness(button, original_text)
 					if (this.$route.query['action']== 'bookmark' && this.$route.query['event_id']) {
 						eventrequests.bookmarkEvent(this.$route.query['event_id']).then(()=> {
 							//this.event.bookmarks = [0] // just add an item to the list so that bookmarks will stop showing
@@ -261,7 +261,7 @@ export default {
 
 				})
 				.catch(err=> {			
-					toggleButtonActiveness(l, original_text)
+					toggleButtonActiveness(button, original_text)
 					this.authError = true;
 					if (err.response && err.response.data && err.response.data.message) {
 

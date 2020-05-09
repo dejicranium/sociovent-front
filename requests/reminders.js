c4af66570cca1cb2eda0b67158c75182253e8b74
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-05-09 22:31:32 +0100

    a

--- requests/reminders.js
+++ requests/reminders.js
@@ -4,14 +4,14 @@ const request = utils.request;
 
 module.exports = {
     googleCalendarShareAPI(){
-        
+
         //const API_URL = "https://calendar.google.com/calendar/r/eventedit?";
         //const description = this.event.description || `${this.event.name} on ${this.event.venue}`;
         //const normalizedStartTime = this.event.start_time.split('-').join('').split('.').join('').split('-').join('').split(':').join('');
         //const query = `text=${this.event.name}&details=${description}&location=${this.event.venue}&dates=${normalizedStartTime}/${normalizedStartTime}`;
         //return API_URL + query;
 
-        return "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events&response_type=code&client_id=522673690521-i1gabu8t5nqongl8g4idcjjf8v5n9cgl.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwww.sociovent.com%2Freminder-stat";
+        return "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.events&response_type=code&client_id=522673690521-i1gabu8t5nqongl8g4idcjjf8v5n9cgl.apps.googleusercontent.com&redirect_uri=https%3A%2F%2F.sociovent.com%2Freminder-stat";
 
     },   
     getReminders: async (query=null) => {
